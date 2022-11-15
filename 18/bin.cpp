4aee5c4398ea33386e9eec7bef2d9147c3717f9f
hy <rlagpdud0524@gmail.com>
2022-11-16 01:32:29 +0900

    .

--- 18/bin.cpp
+++ 18/bin.cpp
@@ -3,39 +3,71 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
-#include <iomanip>
 using namespace std;
 
-int bin[510][510]; // 1,2 : 값의 차이, 첫번째 쓰레기통의 위치
-int arr[510]{ 0 };
+int bin[510]; // 최소값 저장 배열
+int arr[510]{ 0 }; // 값을 받아오는 배열
+bool check[510]{ 0 }; //f로 초기화된 불배열
 int n, m; //n은 집의 갯수, m은 분리수거 통의 갯수
-//배열에 저장된 값을 가져오는 것. 
 
-
+// 계산기 함수(쓰레기 통의 위치를 제외한 나머지 배열 덧셈)
+int total_calc(int min) { //index = 쓰레기 통의 위치
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (!check[i]) sum += bin[i]; // check가 false면 쓰레기통은 배치되지 않았다는 의미
+        if (sum > min) break;
+    }
+    return sum;
+}
+// 빼기 함수
+int miuns_cal(int min) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (check[i]) {
+            for (int j = 0; j < n; j++) {
+                if (!check[j]) {
+                    int tmp = bin[j];
+                    if (bin[j] >= (arr[i] - arr[j]) || bin[j] >= (arr[j] - arr[i])) {
+                        if (arr[i] > arr[j]) bin[j] = arr[i] - arr[j];
+                        else bin[j] = arr[j] - arr[i];
+                        //bin[j] = max(arr[i] - arr[j], arr[j] - arr[i]);
+                    }
+                    else {
+                        if (tmp != bin[j]) sum -= tmp; // 값이 변화되었다면?
+                    }
+                    sum += bin[j];
+                } // 쓰레기 통이 없을 때, 연산
+                else if(j>=i && check[j]) break;
+            }
+            if (min < sum) return -1;
+        }// i 번째가 쓸레기통일때
+    }// i반복
+    return sum;
+}
+int bins() {
+    int min = 51000;
+    do { //조합 반복문 사용
+        fill_n(bin, n, 51000);
+        int tmp = miuns_cal(min);
+        if (tmp != -1) {
+            // if (min > tmp) min = tmp;
+            min = tmp;
+        }
+    } while (prev_permutation(check, check + n)); // 첫번째 
+    return min;
+}
 int main() {
-    freopen("bin.inp", "r", stdin);
-    // freopen("bin.out", "w", stdout);
+    freopen("1.inp", "r", stdin);
+    //freopen("bin.out", "w", stdout);
     int testcase;
     cin >> testcase;
     while (testcase--) {
         cin >> n >> m;
         for (int i = 0; i < n; i++) {
             cin >> arr[i]; // 배열 값 입력.
-            //if (i < m) check[i] = 1;
+            if (i < m) check[i] = 1;
         }
         sort(arr, arr + n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i] > arr[j]) bin[i][j] = bin[j][i] = arr[i] - arr[j];
-                else bin[i][j] = bin[j][i] = arr[j] - arr[i];
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++)
-                cout <<right <<setw(5) << bin[i][j] << " ";
-            cout << endl;
-        }
-        memset(arr, 0, sizeof(arr));
-        memset(bin, 0, sizeof(bin));
+        cout << bins() << "\n";
     }//while-testcase
 }//main
---
+++ 20/drive.cpp
@@ -0,0 +1,64 @@
+// #define _CRT_SECURE_NO_DEPRECATE
+// #define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int grid[110][110][201][2]; // i~j까지, k번 턴을 돌아서 q는 왼쪽에서 오는지 오른쪽에서 오는지 체크
+int grid_down[110][110];
+int grid_left[110][110];
+int m, n, l, g;
+//최소한 길을 안 돌고 추가 연료없는 길을 찾는다.
+//왼쪽-> 오른쪽: 0, 위-> 아래: 1
+void drive() {
+    // 초기값 설정
+    fill(&grid[0][0][0][0], &grid[m][n][200][3], 250);
+    grid[0][0][0][0] = grid[0][0][0][1] = 0; // 시작점에서 사용한 연료는 0이다.
+    for (int i = 0; i < m; i++) {
+        grid[i+1][0][0][1] = grid[i][0][0][1] + grid_down[i][0];
+    }
+    for (int i = 0; i < n; i++) {
+        grid[0][i+1][0][0] = grid[0][i][0][0] + grid_left[0][i];
+    }
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            for (int k = 1; k < 200; k++) {
+                //위 아래 값에서 업데이트(턴 존재)
+                grid[i][j][k][0] = min(grid[i][j - 1][k][0], grid[i][j - 1][k - 1][1]) + grid_left[i][j - 1];
+                grid[i][j][k][1] = min(grid[i - 1][j][k][1], grid[i - 1][j][k - 1][0]) + grid_down[i - 1][j];
+            }
+        }
+    }
+    int result = 210;
+    for (int i = 0; i < 200; i++) {
+        if (g >= grid[m - 1][n - 1][i][0] || g >= grid[m - 1][n - 1][i][1]) {
+            result = min(result,i);
+        }
+    }
+    if (result ==210) cout << - 1<<'\n';
+    else cout << ((m-1)+(n-1)) * l + result << '\n';
+    
+}
+int main() {
+    freopen("drive.inp", "r", stdin);
+    freopen("drive.out","w",stdout);
+    int testcase;
+    cin >> testcase;
+    while (testcase--) {
+         //m,n은 그리드 크기, l은 주행시간 ,g는 초기 연료
+        //모든 칸의 l은 결국 동일, 차이점은 턴을 돌 때의 차이점이다. g는 추가없는 연료
+        cin >> m >> n >> l >> g;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n-1; j++)
+                cin >> grid_left[i][j];
+        }
+        for (int i = 0; i < m-1; i++) {
+            for (int j = 0; j < n; j++) cin >> grid_down[i][j];
+        }
+
+        drive();
+
+        memset(grid_left, 0, sizeof(grid_left));
+        memset(grid_down, 0, sizeof(grid_down));
+    }
+}
